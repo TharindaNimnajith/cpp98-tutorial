3fd6cdddb1e42c3be66db2e71e36f6a5d5024e39
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-15 12:59:05 +0530

    c++20

---
+++ C++ 20/cmake-build-debug/CMakeFiles/C___20.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___20.dir/main.cpp.obj"
+  "C___20.exe"
+  "C___20.exe.manifest"
+  "C___20.lib"
+  "C___20.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
