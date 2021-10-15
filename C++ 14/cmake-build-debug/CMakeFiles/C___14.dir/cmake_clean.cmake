c468ef4182005f12e920052cc4c6935eda7bf4e5
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-15 12:59:14 +0530

    c++14

---
+++ C++ 14/cmake-build-debug/CMakeFiles/C___14.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___14.dir/main.cpp.obj"
+  "C___14.exe"
+  "C___14.exe.manifest"
+  "C___14.lib"
+  "C___14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
